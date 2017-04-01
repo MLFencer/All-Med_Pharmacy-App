3acc7c5d5defef2f85ca7cf2c90142d057045c2b
Mlfencer <bigmacgyver01@gmail.com>
2017-04-01 10:55:21 -0400

    Added Time Class and Started Building Perscription class.

---
+++ app/src/main/java/com/bigmacdev/pharmacy_app/MyTime.java
@@ -0,0 +1,63 @@
+package com.bigmacdev.pharmacy_app;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by dev on 4/1/2017.
+ */
+
+public class MyTime {
+    private int minutes;
+    private int hours;
+
+    public MyTime(){
+        DateFormat df = new SimpleDateFormat("k");
+        Date date = new Date();
+        this.hours = Integer.parseInt(df.format(date));
+        df = new SimpleDateFormat("m");
+        this.minutes = Integer.parseInt(df.format(date));
+    }
+
+    public String printTime(){
+
+        return hours+":"+minutes;
+    }
+
+    public void addMinutes(int extra){
+        minutes+=extra;
+        if (minutes>=60){
+            hours+=minutes/60;
+            minutes=minutes%60;
+            if(hours>=24){
+                hours=hours%24;
+            }
+        }
+    }
+
+    public void addHours(int extra){
+        hours+=extra;
+        if(hours>=24){
+            hours=hours%24;
+        }
+    }
+
+    public int getHours() {
+        return this.hours;
+    }
+
+    public int getMinutes() {
+        return this.minutes;
+    }
+
+    public boolean isEqual(MyTime other){
+        if(other.getHours()==this.hours){
+            if(other.getMinutes()==this.minutes){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
--- app/src/main/java/com/bigmacdev/pharmacy_app/Perscription.java
+++ app/src/main/java/com/bigmacdev/pharmacy_app/Perscription.java
@@ -1,7 +1,83 @@
 package com.bigmacdev.pharmacy_app;
 
 
-public class Perscription {
-    private String patientName;
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Perscription {
+    private String patientName, drugId, perscriberName, scriptWrittenDate, scriptNumber, quantity;
+    private int timeDue;
+    private MyTime time;
+
+    Perscription(String patientName,String drugId,String perscriberName,String scriptWrittenDate,String scriptNumber,String quantity, int timeDue){
+        this.drugId=drugId;
+        this.patientName=patientName;
+        this.perscriberName=perscriberName;
+        this.quantity=quantity;
+        this.scriptWrittenDate=scriptWrittenDate;
+        this.scriptNumber=scriptNumber;
+        this.timeDue=timeDue;
+        time = new MyTime();
+        time.addMinutes(timeDue);
+    }
+
+
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getDrugId() {
+        return drugId;
+    }
+
+    public void setDrugId(String drugId) {
+        this.drugId = drugId;
+    }
+
+    public String getPerscriberName() {
+        return perscriberName;
+    }
+
+    public void setPerscriberName(String perscriberName) {
+        this.perscriberName = perscriberName;
+    }
+
+    public String getScriptWrittenDate() {
+        return scriptWrittenDate;
+    }
+
+    public void setScriptWrittenDate(String scriptWrittenDate) {
+        this.scriptWrittenDate = scriptWrittenDate;
+    }
+
+    public String getScriptNumber() {
+        return scriptNumber;
+    }
+
+    public void setScriptNumber(String scriptNumber) {
+        this.scriptNumber = scriptNumber;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTimeDue() {
+        return timeDue;
+    }
+
+    public void setTimeDue(int timeDue) {
+        this.timeDue = timeDue;
+    }
 }
